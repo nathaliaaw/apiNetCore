a8c769d1ec29d9bda7ef73147baadc04cd019b2d
nacardozog <nacardozog@heon.com.co>
2021-07-01 22:24:30 -0500

    Carga inicial del proyecto

---
+++ apiNet/apiNet/Models/SolicitudContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiNet.Models
+{
+    public class SolicitudContext : DbContext
+    {
+        public SolicitudContext(DbContextOptions<SolicitudContext> options)
+   : base(options)
+        {
+        }
+
+        public DbSet<SolicitudesHogwarts> SolicitudesHogwarts { get; set; }
+    }
+}
---
+++ apiNet/apiNet/Models/SolicitudesHogwarts.cs
@@ -0,0 +1,29 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiNet.Models
+{
+
+    public class SolicitudesHogwarts
+    {
+        [Key]
+        [Required]
+        [Range(1, 9999999999)]
+        public long Identificación { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "El nombre debe contener maximo 20 caracteres")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "El apellido debe contener maximo 20 caracteres")]
+        public string Apellido { get; set; }
+        [Required]
+        [Range(1, 99)]
+        public int Edad { get; set; }
+        [Required]
+        public string CasaHogwarts { get; set; }
+    }
+}
